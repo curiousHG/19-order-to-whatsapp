5abd637b4b33eeeb7fa4d1fa44a7344eb617bcc2
harsh6372 <harshitgupta@arqanum.io>
2023-01-23 02:20:13 +0530

    changed settings.py

--- core/settings.py
+++ core/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
