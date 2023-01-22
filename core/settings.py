8a261d6c7ac6c462aecec453c55169cdd43c7d89
harsh6372 <harshitgupta@arqanum.io>
2023-01-23 03:22:37 +0530

    ran the "python3 manage.py collectstatic"

--- core/settings.py
+++ core/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # print(STATIC_ROOT)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_URL = '/frontend/static/'
 
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 
