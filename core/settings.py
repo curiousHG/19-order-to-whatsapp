9dc9fea9bf3221429e330a00faf32bbf5b249764
harsh6372 <harshitgupta@arqanum.io>
2023-01-23 04:51:42 +0530

    ok

--- core/settings.py
+++ core/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 # STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,"static"),
+   BASE_DIR/"static",
 ]
 # print(STATIC_ROOT)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
