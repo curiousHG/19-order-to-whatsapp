84e3ad27b3a4bd3bd7d410b94fdf6082f8a9721e
harsh6372 <harshitgupta@arqanum.io>
2023-01-23 05:32:23 +0530

    pu

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from django.conf import settings
 from pathlib import Path
 import os
 
@@ -131,10 +131,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles/")
+# STATIC_ROOT = BASE_DIR
 # STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
-   BASE_DIR/"static",
+   os.path.join(BASE_DIR,"static"),
 ]
 # print(STATIC_ROOT)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
