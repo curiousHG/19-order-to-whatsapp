61ec1fbc05a220aaf26bee2e2fe82cff9e0911bb
harsh6372 <harshitgupta@arqanum.io>
2023-01-24 16:05:57 +0530

    railway setup

--- core/settings.py
+++ core/settings.py
@@ -131,16 +131,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles/")
-# STATIC_ROOT = BASE_DIR
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,"static"),
 ]
-# print(STATIC_ROOT)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_URL = 'static/'
 
-STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 
